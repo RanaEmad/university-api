e392be16c2edefe5a3d64d45248548f3f9ec8b5d
Rana Emad <rana3emad@gmail.com>
2019-08-20 20:36:03 +0200

    optimize courses

--- app/Course.php
+++ app/Course.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     protected $table = 'course';
+
+    public function scopeWithAvailibility($query)
+    {
+        return $query->select('course.*',
+         \DB::raw('(Case WHEN COUNT(registration.student_id) < course.capacity THEN "Yes" ELSE "No" END ) as available'))
+        ->leftJoin('registration', 'course.id', '=', 'registration.course_id')
+        ->groupBy("course.id");
+    }
 }
