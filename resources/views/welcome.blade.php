f989541b4d654ec7f6528a1328c14cf6cbed24de
Rana Emad <rana3emad@gmail.com>
2019-08-21 21:32:07 +0200

    create welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,6 +48,12 @@
                 font-size: 84px;
             }
 
+            .links{
+                font-size: 18px;
+                width:80%;
+                margin: 0 auto;
+            }
+
             .links > a {
                 color: #636b6f;
                 padding: 0 25px;
@@ -65,33 +71,15 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    University API
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                This microservice enables students to create an account, login, view courses and register in courses.
+                You can find out more about how to use the endpoints through the README.md file and the Postman documentation in the following link:<br><a href="https://documenter.getpostman.com/view/8472194/SVfGzCRX">https://documenter.getpostman.com/view/8472194/SVfGzCRX</a>
                 </div>
             </div>
         </div>
